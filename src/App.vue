566cbb0b38b086270a82b514e2dcf269febde74f
leeehuixian <leeehuixian@163.com>
2021-01-13 13:35:05 +0800

    调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
+<!--
+ * @Author: LeeHuiXian
+ * @version: 
+ * @Date: 2021-01-12 23:06:32
+ * @LastEditors: LeeHuiXian
+ * @LastEditTime: 2021-01-13 13:32:36
+ * @FilePath: /fed-e-task-03-06/edu-boss-fed/src/App.vue
+ * @Description: 
+-->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <h1>拉勾教育</h1>
+    <!-- 根路由的出口 -->
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+<style lang="scss" scoped>
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,19 @@
+/*
+ * @Author: LeeHuiXian
+ * @version: 
+ * @Date: 2021-01-12 23:06:32
+ * @LastEditors: LeeHuiXian
+ * @LastEditTime: 2021-01-13 13:32:21
+ * @FilePath: /fed-e-task-03-06/edu-boss-fed/src/router/index.ts
+ * @Description: 
+ */
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+// 路由配置规则
+const routes: Array<RouteConfig> = []
 
 const router = new VueRouter({
   routes
